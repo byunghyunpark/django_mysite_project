293684a60b085d6682f99243cc450cf07f6cbf35
byunghyunpark <openmind8735@gmail.com>
2016-10-25 17:29:46 +0900

    Add likeuer and dislikeuser in photo models

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -15,7 +15,20 @@ class Photo(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=100, blank=True)
     img = models.ImageField(upload_to='photo')
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='PhotoLike', related_name='photo_set_like_users')
+    dislike_users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='PhotoDislike', related_name='photo_set_dislike_users')
 
     def __str__(self):
         self.title
 
+
+class PhotoLike(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class PhotoDislike(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
