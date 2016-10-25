c294348fa4ff076b3cc266d323088cdd8557d57c
byunghyunpark <openmind8735@gmail.com>
2016-10-25 16:40:11 +0900

    Add models in photo

--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'video',
     'blog',
     'member',
+    'photo',
 ]
 
 MIDDLEWARE = [
--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=30)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    description = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        self.title
+
+class Photo(models.Model):
+    Album = models.ForeignKey(Album)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100, blank=True)
+    img = models.ImageField(upload_to='photo')
+
+    def __str__(self):
+        self.title
+
