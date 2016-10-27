6852464dd0c982fb917b386a4597689e9186eb6c
byunghyunpark <openmind8735@gmail.com>
2016-10-27 22:15:03 +0900

    Bugfix: rename pk to album_pk

--- django_app/photo/urls.py
+++ django_app/photo/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^album/list/$', views.album_list, name='album_list'),
     url(r'album/add/$', views.album_add, name='album_add'),
     url(r'album/(?P<pk>[0-9]+)/$', views.album_detail, name='album_detail'),
-    url(r'album/upload/(?P<pk>[0-9]+)/$', views.upload_file, name='upload'),
+    url(r'album/upload/(?P<album_pk>[0-9]+)/$', views.upload_file, name='upload'),
     url(r'album/photo/like/(?P<pk>[0-9]+)/$', views.photo_like, name='photo_like'),
     url(r'album/photo/dislike/(?P<pk>[0-9]+)/$', views.photo_dislike, name='photo_dislike'),
 ]
