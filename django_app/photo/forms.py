957af4ff1dbdfb281db7100773b233762886948e
byunghyunpark <openmind8735@gmail.com>
2016-10-27 22:19:40 +0900

    Change form structures.

--- django_app/photo/forms.py
+++ django_app/photo/forms.py
@@ -20,6 +20,21 @@ class AlbumAdd(forms.Form):
     )
 
 class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
-    description = forms.CharField(max_length=100)
-    file = forms.FileField()
+    title = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+            }
+        )
+    )
+    description = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+            }
+        )
+    )
+    # 사진 없으면 error 출력
+    file = forms.FileField(required=True)
